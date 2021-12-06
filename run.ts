0285a566707fc8670d586e061634588ed2b726b9
Christiaan <christiaan@newblack.io>
2021-12-06 22:14:18 +0100

    testing solution 1
    test

--- run.ts
+++ run.ts
@@ -1,9 +1,9 @@
 import { solution1 } from './solutions/1/solution';
-// import { solution2 } from './solutions/2/solution'; */
-//import { solution3 } from './solutions/3/solution';
+// import { solution2 } from './solutions/2/solution'; 
+// import { solution3 } from './solutions/3/solution';
 
 (() => {
     solution1(true);
     // solution2(false);
-    //solution3(false);
+    // solution3(false);
 })();
--- solutions/1/solution.ts
+++ solutions/1/solution.ts
@@ -1,41 +1,29 @@
 import { read } from '@lib/read';
 import { split } from '@lib/split';
 
-const countIncreases = (inputList: number[]): number => {
-    return inputList.reduce(
-        (count, measurement, index) => {
-            if (index === inputList.length - 1) {
-                return count;
-            }
-            return measurement < inputList[index + 1]
-                ? count + 1
-                : count;
-        },
-        0,
-    );
+
+
+function TellenMaar(input: number[]) {
+
+    var totals: number = 0;
+
+    for (let i = 0; i < input.length - 1; i++) {
+
+        if (input[i + 1] > input[i])
+            totals++;
+
+    }
+
+
+    console.log(`checking: ${totals}`); // 7 
+
 }
 
-const threeAveraged = (inputList: number[]): number[] => {
-    return inputList.reduce(
-        (list, measurement, index) => {
-            if (index >= inputList.length - 2) {
-                return list;
-            }
-            return [
-                ...list,
-                measurement +
-                inputList[index + 1] +
-                inputList[index + 2]
-            ];
-        },
-        [] as number[],
-    );
-}
 
 export const solution1 = async (test: boolean) => {
     const input = split(await read(1, { test }))
         .map(Number);
 
-    console.log(`2021-01 part one: ${countIncreases(input)}`); // 1529
-    console.log(`2021-01 part two: ${countIncreases(threeAveraged(input))}`); // 1567
+    console.log(`2021-01 part one: ${TellenMaar(input)}`); // 7 test
+    // console.log(`2021-01 part two: ${countIncreases(threeAveraged(input))}`); // 1567
 }
